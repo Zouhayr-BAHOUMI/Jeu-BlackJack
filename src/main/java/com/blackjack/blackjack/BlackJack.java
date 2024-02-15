46f9c52952d8b95a36cbb8136bd39ec4f31e50b0
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-02-15 10:53:11 +0100

    create mes classes de projet

---
+++ src/main/java/com/blackjack/blackjack/BlackJack.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.blackjack.blackjack;
+
+/**
+ *
+ * @author user
+ */
+public class BlackJack {
+
+    public static void main(String[] args) {
+        System.out.println("helle you are in the game");
+    }
+}
